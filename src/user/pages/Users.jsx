3a4efd1ebba08d9fd636926bbdf79fe5449e4a28
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-17 02:04:30 +0800

    Userlist and UserItem component with dummy db, successfully referenced to each other

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return (
-    <h1>Users works</h1>
-  )
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "Angie Samson",
+      image:
+        "https://www.goodnite.com/wp-content/uploads/2020/03/Places-to-Visit-on-A-Road-Trip-Around-California.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
