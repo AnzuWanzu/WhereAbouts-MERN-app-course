14c4ea42a0ed5f73a351a998d0dcb2da6a40faa0
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-16 19:02:21 +0800

    setup and simple pages and routes for WhereAbouts app

---
+++ src/places/pages/NewPlace.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewPlace = () => {
+  return (
+    <h1>NewPlace works</h1>
+  )
+};
+
+export default NewPlace;
---
+++ src/user/pages/Users.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <h1>Users works</h1>
+  )
+}
+
+export default Users;
